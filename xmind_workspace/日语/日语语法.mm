c5e4eea6135231b0047baf1ade75c107ad8536f2
bing <975772131@qq.com>
2023-12-10 22:31:17 +0800

    add japanese note

---
+++ xmind_workspace/日语/日语语法.mm
@@ -0,0 +1,27 @@
+
+<map>
+  <node ID="root" TEXT="日语语法">
+    <node TEXT="连用形" ID="qXdd9PtTgZ" _mubu_text="%3Cspan%3E%E8%BF%9E%E7%94%A8%E5%BD%A2%3C/span%3E" STYLE="bubble" POSITION="default">
+      <node TEXT="形容词" ID="KNP6sba2db" _mubu_text="%3Cspan%3E%E5%BD%A2%E5%AE%B9%E8%AF%8D%3C/span%3E" STYLE="fork">
+        <node TEXT="い　→　く" ID="9jlJFhsYNC" _mubu_text="%3Cspan%3E%E3%81%84%E3%80%80%E2%86%92%E3%80%80%E3%81%8F%3C/span%3E" STYLE="fork"/>
+        <node TEXT="教室は大きく、静かだ" ID="03Xc5Eq9UU" _mubu_text="%3Cspan%3E%E6%95%99%E5%AE%A4%E3%81%AF%E5%A4%A7%E3%81%8D%E3%81%8F%E3%80%81%E9%9D%99%E3%81%8B%E3%81%A0%3C/span%3E" STYLE="fork"/>
+        <node TEXT="面白くない" ID="zJ9ATUMkaT" _mubu_text="%3Cspan%3E%E9%9D%A2%E7%99%BD%E3%81%8F%E3%81%AA%E3%81%84%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="形容动词" ID="E2mRvXaAuX" _mubu_text="%3Cspan%3E%E5%BD%A2%E5%AE%B9%E5%8A%A8%E8%AF%8D%3C/span%3E" STYLE="fork">
+        <node TEXT="+で" ID="AXjIxqTwc0" _mubu_text="%3Cspan%3E+%E3%81%A7%3C/span%3E" STYLE="fork"/>
+        <node TEXT="教室は静かで、大きい" ID="dXmSd62mqs" _mubu_text="%3Cspan%3E%E6%95%99%E5%AE%A4%E3%81%AF%E9%9D%99%E3%81%8B%E3%81%A7%E3%80%81%E5%A4%A7%E3%81%8D%E3%81%84%3C/span%3E" STYLE="fork"/>
+        <node TEXT="有名ではない" ID="i9y2HtTxEX" _mubu_text="%3Cspan%3E%E6%9C%89%E5%90%8D%E3%81%A7%E3%81%AF%E3%81%AA%E3%81%84%3C/span%3E" STYLE="fork"/>
+      </node>
+      <node TEXT="连体词" ID="VH23Npz7EA" _mubu_text="%3Cspan%3E%E8%BF%9E%E4%BD%93%E8%AF%8D%3C/span%3E" STYLE="fork">
+        <node TEXT="本身就可以直接接名词" ID="PVhKd8DxqM" _mubu_text="%3Cspan%3E%E6%9C%AC%E8%BA%AB%E5%B0%B1%E5%8F%AF%E4%BB%A5%E7%9B%B4%E6%8E%A5%E6%8E%A5%E5%90%8D%E8%AF%8D%3C/span%3E" STYLE="fork"/>
+        <node TEXT="大きな" ID="heFrkMB8Y4" _mubu_text="%3Cspan%3E%E5%A4%A7%E3%81%8D%E3%81%AA%3C/span%3E" STYLE="fork"/>
+        <node TEXT="小さな" ID="2M80aIYURd" _mubu_text="%3Cspan%3E%E5%B0%8F%E3%81%95%E3%81%AA%3C/span%3E" STYLE="fork"/>
+        <node TEXT="色んな" ID="1LauxPcZaW" _mubu_text="%3Cspan%3E%E8%89%B2%E3%82%93%E3%81%AA%3C/span%3E" STYLE="fork"/>
+        <node TEXT="同じ" ID="fiKDuCsj5r" _mubu_text="%3Cspan%3E%E5%90%8C%E3%81%98%3C/span%3E" STYLE="fork">
+          <node TEXT="可以直接作谓语" ID="xRbXAqL7SF" _mubu_text="%3Cspan%3E%E5%8F%AF%E4%BB%A5%E7%9B%B4%E6%8E%A5%E4%BD%9C%E8%B0%93%E8%AF%AD%3C/span%3E" STYLE="fork"/>
+          <node TEXT="同じだ/です" ID="xWOC6JEGOL" _mubu_text="%3Cspan%3E%E5%90%8C%E3%81%98%E3%81%A0/%E3%81%A7%E3%81%99%3C/span%3E" STYLE="fork"/>
+        </node>
+      </node>
+    </node>
+  </node>
+</map>
