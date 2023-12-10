6dc4867afbbc26e3853b739ae9e51c661d365f90
bing <975772131@qq.com>
2023-12-10 22:13:46 +0800

    增加日语学习文档

---
+++ xmind_workspace/日语/日语-新标日.mm
@@ -0,0 +1,34 @@
+
+<map>
+  <node ID="root" TEXT="日语-新标日">
+    <node TEXT="第2单元" ID="DfcDY0ZyPq" _mubu_text="%3Cspan%3E%E7%AC%AC2%E5%8D%95%E5%85%83%3C/span%3E" STYLE="bubble" POSITION="right">
+      <node TEXT="第5课" ID="kFO0KJAMkP" _mubu_text="%3Cspan%3E%E7%AC%AC5%E8%AF%BE%3C/span%3E" STYLE="fork">
+        <node TEXT="名[时间]に动" ID="WcmTBLnmgd" _mubu_text="%3Cspan%3E%E5%90%8D%5B%E6%97%B6%E9%97%B4%5D%E3%81%AB%E5%8A%A8%3C/span%3E" STYLE="fork">
+          <node TEXT="动作发生的时间" ID="pJ6emRAPil" _mubu_text="%3Cspan%3E%E5%8A%A8%E4%BD%9C%E5%8F%91%E7%94%9F%E7%9A%84%E6%97%B6%E9%97%B4%3C/span%3E" STYLE="fork"/>
+          <node TEXT="包含数字的时间后才加" ID="4OGj5oEg1E" _mubu_text="%3Cspan%3E%E5%8C%85%E5%90%AB%E6%95%B0%E5%AD%97%E7%9A%84%E6%97%B6%E9%97%B4%E5%90%8E%E6%89%8D%E5%8A%A0%3C/span%3E" STYLE="fork"/>
+          <node TEXT="星期后可加可不加" ID="xRyuoUP2co" _mubu_text="%3Cspan%3E%E6%98%9F%E6%9C%9F%E5%90%8E%E5%8F%AF%E5%8A%A0%E5%8F%AF%E4%B8%8D%E5%8A%A0%3C/span%3E" STYLE="fork"/>
+        </node>
+      </node>
+      <node TEXT="第7课" ID="pRC6RDV2Fi" _mubu_text="%3Cspan%3E%E7%AC%AC7%E8%AF%BE%3C/span%3E" STYLE="fork">
+        <node TEXT="名[场所]で动" ID="1SRgeQ4LxP" _mubu_text="%3Cspan%3E%E5%90%8D%5B%E5%9C%BA%E6%89%80%5D%E3%81%A7%E5%8A%A8%3C/span%3E" STYLE="fork">
+          <node TEXT="动作进行的场所" ID="SLvdx1otLx" _mubu_text="%3Cspan%3E%E5%8A%A8%E4%BD%9C%E8%BF%9B%E8%A1%8C%E7%9A%84%E5%9C%BA%E6%89%80%3C/span%3E" STYLE="fork"/>
+          <node TEXT="存在的场所用に" ID="Ta1pgTFp5M" _mubu_text="%3Cspan%3E%E5%AD%98%E5%9C%A8%E7%9A%84%E5%9C%BA%E6%89%80%E7%94%A8%E3%81%AB%3C/span%3E" STYLE="fork"/>
+        </node>
+      </node>
+    </node>
+    <node TEXT="第3单元" ID="zlSoYRfhMC" _mubu_text="%3Cspan%3E%E7%AC%AC3%E5%8D%95%E5%85%83%3C/span%3E" STYLE="bubble" POSITION="left">
+      <node TEXT="第9课" ID="K4ES5O5iJs" _mubu_text="%3Cspan%3E%E7%AC%AC9%E8%AF%BE%3C/span%3E" STYLE="fork">
+        <node TEXT="一形谓语否定" ID="TGRQgZUcK9" _mubu_text="%3Cspan%3E%E4%B8%80%E5%BD%A2%E8%B0%93%E8%AF%AD%E5%90%A6%E5%AE%9A%3C/span%3E" STYLE="fork">
+          <node TEXT="い -&gt; く＋ない" ID="bB3v67IAUi" _mubu_text="%3Cspan%3E%E3%81%84%20-&amp;gt;%20%E3%81%8F%EF%BC%8B%E3%81%AA%E3%81%84%3C/span%3E" STYLE="fork"/>
+          <node TEXT="いいです -&gt; よくないです " ID="6ZMPVPJigG" _mubu_text="%3Cspan%3E%E3%81%84%E3%81%84%E3%81%A7%E3%81%99%20-&amp;gt;%20%E3%82%88%E3%81%8F%E3%81%AA%E3%81%84%E3%81%A7%E3%81%99%20%3C/span%3E" STYLE="fork"/>
+        </node>
+        <node TEXT="一形谓语过去式" ID="JnNOYtqkma" _mubu_text="%3Cspan%3E%E4%B8%80%E5%BD%A2%E8%B0%93%E8%AF%AD%E8%BF%87%E5%8E%BB%E5%BC%8F%3C/span%3E" STYLE="fork">
+          <node TEXT="い　→　かった" ID="70Awjugdb1" _mubu_text="%3Cspan%3E%E3%81%84%E3%80%80%E2%86%92%E3%80%80%E3%81%8B%E3%81%A3%E3%81%9F%3C/span%3E" STYLE="fork"/>
+        </node>
+        <node TEXT="一形谓语过去否定" ID="ODEjkvRVHP" _mubu_text="%3Cspan%3E%E4%B8%80%E5%BD%A2%E8%B0%93%E8%AF%AD%E8%BF%87%E5%8E%BB%E5%90%A6%E5%AE%9A%3C/span%3E" STYLE="fork">
+          <node TEXT="い　→　くなかったです / くありませんでした" ID="M5HfZMn1KV" _mubu_text="%3Cspan%3E%E3%81%84%E3%80%80%E2%86%92%E3%80%80%E3%81%8F%E3%81%AA%E3%81%8B%E3%81%A3%E3%81%9F%E3%81%A7%E3%81%99%20/%20%E3%81%8F%E3%81%82%E3%82%8A%E3%81%BE%E3%81%9B%E3%82%93%E3%81%A7%E3%81%97%E3%81%9F%3C/span%3E" STYLE="fork"/>
+        </node>
+      </node>
+    </node>
+  </node>
+</map>
